9b095268d8fe8d1436eb56f17d6e7888091b9e07
mu1101i <mulloli@me.com>
2012-11-22 17:33:24 +0530

    Initial Commit

---
+++ MicroPDF/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MicroPDF
+//
+//  Created by Varun Mulloli on 22/11/12.
+//  Copyright (c) 2012 Varun Mulloli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MicroPDF/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  MicroPDF
+//
+//  Created by Varun Mulloli on 22/11/12.
+//  Copyright (c) 2012 Varun Mulloli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
